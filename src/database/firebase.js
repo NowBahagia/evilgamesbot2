aad26ae603fc31e5fa352fdbdfcbe3c89d1c92b1
NowBahagia <lisadesiana45@gmail.com>
2024-07-30 19:20:05 +0700

    Update firebase.js

--- src/database/firebase.js
+++ src/database/firebase.js
@@ -3,7 +3,13 @@ import { getFirestore } from 'firebase/firestore';
 
 
 const firebaseConfig = {
-  "Your Firebase Private Keys Here"
+  apiKey: "AIzaSyDMmR3sh6iHu37TzytOQJUnKUZqQui42oA",
+  authDomain: "test-818fd.firebaseapp.com",
+  projectId: "test-818fd",
+  storageBucket: "test-818fd.appspot.com",
+  messagingSenderId: "946686728972",
+  appId: "1:946686728972:web:6e875b5ffb05e39a1a0729",
+  measurementId: "G-4ZKZJN8YN3"
 };
 
 const app = initializeApp(firebaseConfig);
